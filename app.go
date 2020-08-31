6fac09fbb2db7e4bd87ccb30a358446ae41356c6
Tatsuemon <i10mann-110@ezweb.ne.jp>
2020-08-31 23:54:44 +0900

    add app.go initialize

--- app.go
+++ app.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
-    _ "github.com/go-sql-driver/mysql"
 )
 
 type App struct {
@@ -11,5 +14,17 @@ type App struct {
 	DB	   *sql.DB
 }
 
-func (a *App) Initialize(user, password, dbname string) {}
+func (a *App) Initialize(user, password, dbname string) {
+	connectionString := fmt.Sprintf("%s:$s@/%s", user, password, dbname)
+	
+	// DBとの接続
+	var err error
+	a.DB, err = sql.Open("mysql", connectionString)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Routerの初期化
+	a.Router = mux.NewRouter()
+}
+
 func (a *App) Run(addr string) {}
